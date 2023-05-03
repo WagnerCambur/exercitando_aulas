4cd3660e5845812bcbc780faa8426af66a8388db
WagnerCambur <wagner.cambur@luizalabs.com>
2023-05-03 11:31:38 -0300

    Composição de Classes

---
+++ oo/sql_actions/repositorio.py
@@ -0,0 +1,11 @@
+from insert import Insert
+from select import Select
+#Orientação a Objetos em Python - Composição de Classes
+class Repositorio:
+
+    def __init__ (self) -> None:
+        self.__insert = Insert()
+        self.__select = Select()
+    
+    def select_by_id(self):
+        self.__select.select_one()
